0145576297e388686dc62f53891bdd214bbfc4bd
arjunUpatel <arjunpatel151002@gmail.com>
2022-01-05 22:24:18 -0500

    Backtracking state tree working

--- java/GenerateParentheses.java
+++ java/GenerateParentheses.java
@@ -2,25 +2,26 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class GenerateParentheses {
-	public List<String> generateParenthesis(int n) {
-		List<String> res = new ArrayList<>();
-		ParenthesisNode root = createStateTree(new ParenthesisNode(), 0, 0);
+	public static void main(String[] args) {
+		generateParenthesis(2);
 	}
 
-	public ParenthesisNode createStateTree(ParenthesisNode node, int leftCount, int rightCount){
-		if(leftCount < 3) {
-			node.left.type = -1;
-			node.left = createStateTree(node.left, leftCount++, rightCount);
-		}
-		if(rightCount < 3) {
-			node.right.type = 1;
-			node.right = createStateTree(node.left, rightCount, leftCount);
-		}
+	public static List<String> generateParenthesis(int n) {
+		List<String> res = new ArrayList<>();
+		ParenthesisNode root = createStateTree(new ParenthesisNode((char) 0, null, null), 0, 0, n);
+		return res;
+	}
+
+	public static ParenthesisNode createStateTree(ParenthesisNode node, int leftCount, int rightCount, int n) {
+		if (leftCount < n)
+			node.left = createStateTree(new ParenthesisNode('('), leftCount + 1, rightCount, n);
+		if (rightCount < n)
+			node.right = createStateTree(new ParenthesisNode(')'), leftCount, rightCount + 1, n);
 		return node;
 	}
 
-	class ParenthesisNode {
-		int type;
+	static class ParenthesisNode {
+		char type;
 		ParenthesisNode left;
 		ParenthesisNode right;
 
@@ -28,11 +29,11 @@ public class GenerateParentheses {
 
 		}
 
-		public ParenthesisNode(int type) {
+		public ParenthesisNode(char type) {
 			this.type = type;
 		}
 
-		public ParenthesisNode(int type, ParenthesisNode left, ParenthesisNode right) {
+		public ParenthesisNode(char type, ParenthesisNode left, ParenthesisNode right) {
 			this.type = type;
 			this.left = left;
 			this.right = right;
