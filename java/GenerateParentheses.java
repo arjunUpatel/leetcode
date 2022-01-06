71696fc439769d09df659fc75174bd111658d0ad
arjunUpatel <arjunpatel151002@gmail.com>
2022-01-06 13:30:54 -0500

    Solved question

--- java/GenerateParentheses.java
+++ java/GenerateParentheses.java
@@ -2,41 +2,21 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class GenerateParentheses {
-	public static void main(String[] args) {
-		generateParenthesis(2);
-	}
+	List<String> res = new ArrayList<>();
 
-	public static List<String> generateParenthesis(int n) {
-		List<String> res = new ArrayList<>();
-		ParenthesisNode root = createStateTree(new ParenthesisNode((char) 0, null, null), 0, 0, n);
+	public List<String> generateParenthesis(int n) {
+		backtrack(res, "", 0, 0, n);
 		return res;
 	}
 
-	public static ParenthesisNode createStateTree(ParenthesisNode node, int leftCount, int rightCount, int n) {
+	public static void backtrack(List<String> list, String str, int leftCount, int rightCount, int n) {
+		if (str.length() == 2 * n) {
+			list.add(str);
+			return;
+		}
 		if (leftCount < n)
-			node.left = createStateTree(new ParenthesisNode('('), leftCount + 1, rightCount, n);
-		if (rightCount < n)
-			node.right = createStateTree(new ParenthesisNode(')'), leftCount, rightCount + 1, n);
-		return node;
-	}
-
-	static class ParenthesisNode {
-		char type;
-		ParenthesisNode left;
-		ParenthesisNode right;
-
-		public ParenthesisNode() {
-
-		}
-
-		public ParenthesisNode(char type) {
-			this.type = type;
-		}
-
-		public ParenthesisNode(char type, ParenthesisNode left, ParenthesisNode right) {
-			this.type = type;
-			this.left = left;
-			this.right = right;
-		}
+			backtrack(list, str + '(', leftCount + 1, rightCount, n);
+		if (rightCount < leftCount)
+			backtrack(list, str + ')', leftCount, rightCount + 1, n);
 	}
 }
