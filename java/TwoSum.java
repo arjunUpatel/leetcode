c7d9e13f7880c008e8cd7202a1ad4220bc859858
Arjun Patel <arjunpatel151002@gmail.com>
2022-05-13 22:09:06 -0400

    Refactored code - 1. Two Sum

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -3,18 +3,14 @@ import java.util.HashMap;
 class TwoSum {
 	public int[] twoSum(int[] nums, int target) {
 		HashMap<Integer, Integer> map = new HashMap<>();
-		int[] res = new int[2];
 		for (int i = 0; i < nums.length; i++) {
 			int key = nums[i];
 			int complement = target - key;
-			if (map.containsKey(complement)) {
-				res[0] = map.get(complement);
-				res[1] = i;
-				return res;
-			}
+			if (map.containsKey(complement))
+				return new int[] { map.get(complement), i };
 			if (!map.containsKey(key))
 				map.put(key, i);
 		}
-		return res;
+		return new int[] {};
 	}
 }
