a50bcb19d6f804baff58ca4c8095a2b73511ee27
Arjun Patel <arjunpatel151002@gmail.com>
2022-05-21 23:33:04 -0400

    Solved question - 383. Ransom Note

---
+++ java/RansomNote.java
@@ -0,0 +1,14 @@
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for (int i = 0; i < ransomNote.length(); i++)
+            arr[ransomNote.charAt(i) - 97]++;
+        for (int i = 0; i < magazine.length(); i++)
+            arr[magazine.charAt(i) - 97]--;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0)
+                return false;
+        }
+        return true;
+    }
+}
